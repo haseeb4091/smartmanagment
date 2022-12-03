722bf473e8e081e75423f479394a9da437fd8eac
haseeb4091 <jasialnew1@gmail.com>
2022-12-03 15:43:50 +0500

    ngzoroo

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import { NzGridModule } from 'ng-zorro-antd/grid';
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
 })
+
 export class WelcomeComponent implements OnInit {
 
   constructor() { }
--- src/app/pages/welcome/welcome.module.ts
+++ src/app/pages/welcome/welcome.module.ts
@@ -1,13 +1,12 @@
-import { NgModule } from '@angular/core';
 
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { WelcomeRoutingModule } from './welcome-routing.module';
-
 import { WelcomeComponent } from './welcome.component';
-
-
+import { NzGridModule } from 'ng-zorro-antd/grid';
 @NgModule({
-  imports: [WelcomeRoutingModule],
+  imports: [WelcomeRoutingModule,NzGridModule],
   declarations: [WelcomeComponent],
-  exports: [WelcomeComponent]
+  exports: [WelcomeComponent],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class WelcomeModule { }
